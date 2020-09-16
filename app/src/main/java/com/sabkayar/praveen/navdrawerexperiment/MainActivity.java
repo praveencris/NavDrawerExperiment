66618304c101f6935ce0a42676941741852cbdcc
Praveen <pkchmits@gmail.com>
2020-09-16 16:30:21 +0530

    1. Done adding Navigation Drawer 2. Learnt the placement of NavigationView in DrawerLayout 3. Same way BottomBar can also be implemented

--- app/src/main/java/com/sabkayar/praveen/navdrawerexperiment/MainActivity.java
+++ app/src/main/java/com/sabkayar/praveen/navdrawerexperiment/MainActivity.java
@@ -1,14 +1,135 @@
 package com.sabkayar.praveen.navdrawerexperiment;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
+import com.sabkayar.praveen.navdrawerexperiment.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private ActivityMainBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
+
+        //Set a Toolbar to act as the ActionBar for this Activity window.
+        setSupportActionBar(mBinding.appBarMainLayout.toolbar);
+
+
+        mBinding.appBarMainLayout.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(mBinding.appBarMainLayout.appBar, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
+        //-----------Navigation Drawer--------
+        ActionBarDrawerToggle drawerToggle =
+                new ActionBarDrawerToggle(
+                        this, mBinding.drawerLayout, mBinding.appBarMainLayout.toolbar,
+                        R.string.navigation_drawer_open, R.string.navigation_drawer_close
+                );
+
+        mBinding.drawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+
+        mBinding.navView.setNavigationItemSelectedListener(this);
+        //-----------Navigation Drawer--------
+    }
+
+    /**
+     * Handles a navigation drawer item click. It detects which item was
+     * clicked and displays a toast message showing which item.
+     *
+     * @param item Item in the navigation drawer
+     * @return Returns true after closing the nav drawer
+     */
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // Handle navigation view item clicks here.
+        switch (item.getItemId()) {
+            case R.id.nav_camera:
+                // Handle the camera import action (for now display a toast).
+                mBinding.drawerLayout.closeDrawer(GravityCompat.START);
+                displayToast(getString(R.string.chose_camera));
+                return true;
+            case R.id.nav_gallery:
+                // Handle the gallery action (for now display a toast).
+                mBinding.drawerLayout.closeDrawer(GravityCompat.START);
+                displayToast(getString(R.string.chose_gallery));
+                return true;
+            case R.id.nav_slideshow:
+                // Handle the slideshow action (for now display a toast).
+                mBinding.drawerLayout.closeDrawer(GravityCompat.START);
+                displayToast(getString(R.string.chose_slideshow));
+                return true;
+            case R.id.nav_manage:
+                // Handle the tools action (for now display a toast).
+                mBinding.drawerLayout.closeDrawer(GravityCompat.START);
+                displayToast(getString(R.string.chose_tools));
+                return true;
+            case R.id.nav_share:
+                // Handle the share action (for now display a toast).
+                mBinding.drawerLayout.closeDrawer(GravityCompat.START);
+                displayToast(getString(R.string.chose_share));
+                return true;
+            case R.id.nav_send:
+                // Handle the send action (for now display a toast).
+                mBinding.drawerLayout.closeDrawer(GravityCompat.START);
+                displayToast(getString(R.string.chose_send));
+                return true;
+            default:
+                return false;
+
+        }
+    }
+
+    /**
+     * Handles the Back button: closes the nav drawer.
+     */
+    @Override
+    public void onBackPressed() {
+        if (mBinding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            mBinding.drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    /**
+     * Inflates the options menu.
+     *
+     * @param menu Menu to inflate
+     * @return Returns true if menu is inflated.
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.nav_menu, menu);
+        return true;
+    }
+
+    /**
+     * Displays a toast message.
+     *
+     * @param message Message to display in toast
+     */
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
     }
 }
